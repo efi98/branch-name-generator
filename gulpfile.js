ab600a0eac304fededb526d7c8601214a8d10f87
efraim.gelder <efraim.gelder@brainlab.com>
2023-10-17 20:24:05 +0300

    convert to node with TS

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const browserSync = require("browser-sync").create();
+const cleanhtml = require("gulp-cleanhtml");
+const server = require("gulp-webserver");
+const uglify = require('gulp-uglify-es').default;
+
+const tsProject = ts.createProject("tsconfig.json");
+
+// Compile TypeScript files
+gulp.task("compile-ts", () => {
+  return tsProject.src().pipe(tsProject()).js.pipe(uglify()).pipe(gulp.dest("dist"));
+});
+
+// Process Sass and CSS
+gulp.task("compile-sass", () => {
+  return gulp
+    .src("src/styles/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("styles.css"))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("dist"));
+});
+
+// Copy HTML and assets
+gulp.task("copy-html", () => {
+  return gulp.src("src/index.html").pipe(cleanhtml()).pipe(gulp.dest("dist"));
+});
+
+gulp.task("copy-assets", () => {
+  return gulp.src("src/assets/**/*.*").pipe(gulp.dest("dist/assets"));
+});
+
+// Watch for changes and reload the browser
+gulp.task("watch", () => {
+  gulp.src("dist").pipe(
+    server({
+      host: 'localhost',
+      path: '/',
+      livereload: true,
+      open: true,
+      port: 3000, // set a port to avoid conflicts with other local apps
+    })
+  );
+
+  gulp.watch("src/**/*.ts", gulp.series("compile-ts"));
+  gulp.watch("src/**/*.scss", gulp.series("compile-sass"));
+  gulp.watch("src/index.html", gulp.series("copy-html"));
+  gulp.watch("src/assets/**/*.*", gulp.series("copy-assets"));
+  gulp.watch("dist/**/*.*").on("change", browserSync.reload);
+});
+
+// Default task
+gulp.task(
+  "default",
+  gulp.parallel(
+    "compile-ts",
+    "compile-sass",
+    "copy-html",
+    "copy-assets",
+    "watch"
+  )
+);
+
+// Default task
+gulp.task(
+  "prod",
+  gulp.parallel(
+    "compile-ts",
+    "compile-sass",
+    "copy-html",
+    "copy-assets"
+  )
+);
