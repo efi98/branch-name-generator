14a240cf1ef9ea3e357645815ecfecdf745f2b01
efraim.gelder <119013869+efi98@users.noreply.github.com>
2024-04-07 21:51:15 +0300

    added the result logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,19 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
+        const initialMessage: string =
+            '//   _____                _           _   _             _____ __ _ \n' +
+            '//  /  __ \\              | |         | | | |           |  ___/ _(_)\n' +
+            '//  | /  \\/_ __ ___  __ _| |_ ___  __| | | |__  _   _  | |__| |_ _ \n' +
+            '//  | |   | \'__/ _ \\/ _` | __/ _ \\/ _` | | \'_ \\| | | | |  __|  _| |\n' +
+            '//  | \\__/\\ | |  __/ (_| | ||  __/ (_| | | |_) | |_| | | |__| | | |\n' +
+            '//   \\____/_|  \\___|\\__,_|\\__\\___|\\__,_| |_.__/ \\__, | \\____/_| |_|\n' +
+            '//                                               __/ |             \n' +
+            '//                                              |___/              \n'
+            + '//  Repo: https://github.com/efi98/branch-name-generator'
         this.primengConfig.ripple = true;
         this.showWelcomeMessage();
+        console.log(initialMessage);
     }
 
     private showWelcomeMessage() {
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from "@angular/core";
 import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 import {ConfirmationService, MessageService} from "primeng/api";
-import {FieldType, ParsedWorkItem, templateWorkItemFormat, workItemTypes} from "@app-utils";
+import {branchNameConf, FieldType, ParsedWorkItem, templateWorkItemFormat, workItemTypes} from "@app-utils";
+import {debounceTime, distinctUntilChanged} from "rxjs";
 
 @Component({
     selector: "app-main",
@@ -10,7 +11,9 @@ import {FieldType, ParsedWorkItem, templateWorkItemFormat, workItemTypes} from "
 })
 export class MainComponent implements OnInit {
     generatorForm!: FormGroup;
-    parsedWorkItem!: ParsedWorkItem;
+    branchNameResult!: { key: string, value: string }[];
+    private parsedWorkItem!: ParsedWorkItem;
+    private parsedRequirement!: { number: number, title: string };
 
     constructor(private formBuilder: FormBuilder, private confirmationService: ConfirmationService, private messageService: MessageService) {
     }
@@ -25,7 +28,7 @@ export class MainComponent implements OnInit {
         this.generatorForm.get('workItem')?.valueChanges
             .subscribe((workItemValue: string) => {
                 if (this.generatorForm.get('workItem')?.valid) {
-                    this.parseWorkItem();
+                    this.parseWorkItem(workItemValue);
                     this.handleWorkItemsByType();
                 } else {
                     this.generatorForm.get('requirement')?.disable({emitEvent: false});
@@ -35,6 +38,12 @@ export class MainComponent implements OnInit {
         this.generatorForm.get('isReqIncluded')?.valueChanges
             .subscribe((isReqIncludedValue: boolean) => {
                 this.generatorForm.get('requirement')?.[isReqIncludedValue ? 'enable' : 'disable']({emitEvent: false});
+            });
+        this.generatorForm.get('requirement')?.valueChanges.pipe(debounceTime(200), distinctUntilChanged())
+            .subscribe((requirement: string) => {
+                if (this.generatorForm.get('requirement')?.valid) {
+                    this.parseReq(requirement);
+                }
             });
     }
 
@@ -48,7 +57,47 @@ export class MainComponent implements OnInit {
     }
 
     onSubmit() {
-        //
+        this.branchNameResult = [];
+        switch (this.parsedWorkItem.type) {
+            case workItemTypes.Requirement: {
+                const {number, title} = this.parsedWorkItem;
+                this.branchNameResult.push({
+                    key: workItemTypes.Requirement,
+                    value: branchNameConf.Requirement.setBranchName(number.toString(), title)
+                });
+                break;
+            }
+            case workItemTypes.Task: {
+                this.branchNameResult.push({
+                    key: workItemTypes.Requirement,
+                    value: branchNameConf.Requirement.setBranchName(this.parsedRequirement.number, this.parsedRequirement.title)
+                });
+                this.branchNameResult.push({
+                    key: workItemTypes.Task,
+                    value: branchNameConf.Task.setBranchName(this.parsedRequirement.number, this.parsedRequirement.title, this.parsedWorkItem.number, this.parsedWorkItem.title)
+                });
+                break;
+            }
+            case workItemTypes.Bug: {
+                const {number, title} = this.parsedWorkItem;
+                if (this.generatorForm.get('isReqIncluded')?.value) {
+                    this.branchNameResult.push({
+                        key: workItemTypes.Requirement,
+                        value: branchNameConf.Requirement.setBranchName(this.parsedRequirement.number, this.parsedRequirement.title)
+                    });
+                    this.branchNameResult.push({
+                        key: workItemTypes.Bug,
+                        value: branchNameConf.Bug.setBranchName(number, title, this.parsedRequirement.number, this.parsedRequirement.title)
+                    });
+                } else {
+                    this.branchNameResult.push({
+                        key: workItemTypes.Bug,
+                        value: branchNameConf.Bug.setBranchName(number, title),
+                    });
+                }
+                break;
+            }
+        }
     }
 
     showWorkItemFormatModal() {
@@ -78,17 +127,28 @@ export class MainComponent implements OnInit {
         };
     }
 
-    private parseWorkItem() {
-        const workItemValue = this.generatorForm.get('workItem')?.value;
+    private parseWorkItem(workItemValue: string) {
         const regexMatch: any = workItemValue.match(/^(Bug|Task|Requirement)\s(\d{5}):\s([^*^\\:?~\u05D0-\u05EA]+)$/);
         if (regexMatch) {
             const [_, type, number, title] = regexMatch;
             this.parsedWorkItem = {
                 type: type as workItemTypes,
                 number: parseInt(number),
-                title: this.replaceSpacesWithHyphen(title)
+                title: this.formatTitleWithHyphens(title)
             };
             console.log('parsedWorkItem: ', this.parsedWorkItem);
+        }
+    }
+
+    private parseReq(reqValue: string) {
+        const regexMatch: any = reqValue.match(/^Requirement\s(\d{5}):\s([^*^\\:?~\u05D0-\u05EA]+)$/);
+        if (regexMatch) {
+            const [_, number, title] = regexMatch;
+            this.parsedRequirement = {
+                number: parseInt(number),
+                title: this.formatTitleWithHyphens(title)
+            };
+            console.log('parsedReq: ', this.parsedRequirement);
         }
     }
 
@@ -113,10 +173,12 @@ export class MainComponent implements OnInit {
         }
     }
 
-    private replaceSpacesWithHyphen(title: any): string {
-        return title.replace(/[\s_]+/g, '-').replace(/-+/g, '-');
+    private formatTitleWithHyphens(title: any): string {
+        return title.replace(/[\s_]+/g, '-').replace(/-+/g, '-').replace(/\/+/g, '/').replace(/[-\/]+$/, '');
     }
 }
 
 // todo:
-// add alert when: 1) user submit, 2) user changed teh form after generating
+// add alert when: 1) user submit, 2) user changed the form after generating
+// add theme switch
+// add limit to title
