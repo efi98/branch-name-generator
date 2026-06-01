f6b5a94c1a25e162c822f53363cc19f316a41098
Efi <119013869+efi98@users.noreply.github.com>
2026-06-01 14:46:22 +0300

    refactor: Improve code formatting and organization

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from "@angular/forms";
-import { Router } from "@angular/router";
-import { DEFAULTS, mode, stringToBoolean, switchPrimeTheme, theme, USER_THEME } from "@app-utils";
-import { ConfirmationService, MessageService } from "primeng/api";
-import { startWith } from "rxjs";
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DEFAULTS, mode, stringToBoolean, switchPrimeTheme, theme, USER_THEME } from '@app-utils';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { startWith } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
